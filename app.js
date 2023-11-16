304e983bf0079f942d6259e2d656fc640c49cbaa
hanleymark <home@markhanley.co.uk>
2023-11-16 18:23:58 +0000

    Add click handlers and refactor adding elements dynamically

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { botSvg, userSvg } from './avatars.js';
 
 const apiUrl = 'https://api.openai.com/v1/chat/completions';
 const messages = [];
+const history = [];
 
 const inputButtonSubmit = async () => {
   const inputText = document.getElementById('input-text').value;
@@ -17,17 +18,17 @@ const inputButtonSubmit = async () => {
   console.log(`Submitting input: ${inputText}`);
 
   const response = await fetch(apiUrl, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages: messages,
-        stream: false,
-      }),
-    })
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      model: 'gpt-3.5-turbo',
+      messages: messages,
+      stream: false,
+    }),
+  })
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
@@ -36,11 +37,17 @@ const inputButtonSubmit = async () => {
         role: 'assistant',
         content: response,
       });
-    
-      updateDisplay();
+
+      history.push({
+        question: inputText,
+        answer: response,
+      });
+
+      updateMessagesDisplay(messages);
+      updateHistoryDisplay(history);
     })
     .catch((error) => {
-      console.error("Error:", error);
+      console.error('Error:', error);
     });
 };
 
@@ -48,16 +55,34 @@ const clearButtonSubmit = () => {
   console.log('Clear button pressed');
 };
 
-const updateDisplay = () => {
-    const display = document.getElementById('content-messages');
-    display.innerHTML = messages.map((message) => {
-        return `
-        <div class="message-wrapper role-${message.role}">
-          ${message.role === 'user' ? userSvg : botSvg}
-          ${message.content}
-        </div>`;
-    }).join('');
-    }
+const updateMessagesDisplay = (messages) => {
+  clearMessages();
+  const display = document.getElementById('content-messages');
+  messages.forEach((message) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerHTML = `${message.role === 'user' ? userSvg : botSvg}`;
+    messageElement.innerHTML += message.content;
+    messageElement.className = `message-wrapper role-${message.role}`;
+    display.appendChild(messageElement);
+  });
+};
+
+const updateHistoryDisplay = (history) => {
+  clearHistory();
+  const display = document.getElementById('content-history');
+
+  history.forEach((item, index) => {
+    const historyElement = document.createElement('div');
+    const historyItemText = item.question.length > 24 ? `${item.question.substring(0, 21)}...` : item.question;
+    historyElement.innerHTML = `${index + 1} ${historyItemText}`;
+    historyElement.className = 'history-wrapper';
+    historyElement.itemIndex = index;
+    historyElement.addEventListener('click', (e) => {
+      filterMessages(e.target.itemIndex);
+    });
+    display.appendChild(historyElement);
+  });
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   const inputButton = document.querySelector('#input-button');
@@ -69,6 +94,22 @@ document.addEventListener('DOMContentLoaded', () => {
     clearButton.addEventListener('click', clearButtonSubmit);
   }
 });
+
+const clearMessages = () => {
+  document
+    .querySelectorAll('.message-wrapper')
+    .forEach((element) => element.remove());
+};
+
+const clearHistory = () => {
+  document
+    .querySelectorAll('.history-wrapper')
+    .forEach((element) => element.remove());
+};
+
+const filterMessages = (index) => {
+  updateMessagesDisplay(messages.slice(0, index + 1));
+};
 
 // This has to come after the DOMContentLoaded event listener is added
 // otherwise the 'await' may cause the DOMContentLoaded event to be missed
